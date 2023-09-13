78310b053ca62a2f69db2468daa3063e0115f81d
Bishwajit93 <bish.karm123@gmail.com>
2023-09-14 00:23:03 +0200

    userRegistration and userLogin backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,59 @@
+import { validationResult } from 'express-validator';
 import pool from '../config/db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 export const registerUser = async (req, res) => {
-  const { firstName, lastName, username, email, institutionName, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { firstName, lastName, username, email, institutionName, password } = req.body;
 
-  try {
-    const newUser = await pool.query(
-      `INSERT INTO users (first_name, last_name, username, email, institution_name, password)
-      VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
-      [firstName, lastName, username, email, institutionName, password]
-    );
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-    res.json(newUser.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
+        const newUser = await pool.query(
+        `INSERT INTO users (first_name, last_name, username, email, institution_name, password)
+        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+        [firstName, lastName, username, email, institutionName, hashedPassword]
+        );
+
+        res.json(newUser.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
 };
+
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+  
+      if (user.rows.length === 0) {
+        return res.status(401).json("Invalid Credential");
+      }
+  
+      const validPassword = await bcrypt.compare(password, user.rows[0].password);
+  
+      if (!validPassword) {
+        return res.status(401).json("Invalid Credential");
+      }
+      console.log(process.env.JWT_SECRET)
+      const jwtToken = jwt.sign(
+        { user_id: user.rows[0].id },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+  
+      return res.json({ jwtToken });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { registerUser } from '../controllers/userController.js';
+import { check } from 'express-validator';
+import { registerUser, loginUser  } from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/register', [
+    check('firstName', 'First name is required').not().isEmpty(),
+    check('lastName', 'Last name is required').not().isEmpty(),
+    check('username', 'Username is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6, max: 50  }),
+  ], registerUser);
+
+
+router.post('/login', loginUser);
 
 export default router;
